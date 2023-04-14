5fa260a26773ef3efbe29a007b5586c38a8aa1ea
Joel Diharce <joeldiharce@me.com>
2023-04-13 23:54:07 -0500

    Added more 8kyu exercises

---
+++ CodeWars_Kata/Kata006_Convert_boolean_to_YNStrings.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//Given
+
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No';   // Remember to always return something when writing a function.
+}
+
+// Test
+console.log(boolToWord(true))
+console.log(boolToWord(false))
---
+++ CodeWars_Kata/Kata009_Sum_of_Positive.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// Given: 
+
+function positiveSum(arr) {
+  let positives = [];
+    arr.forEach(
+      function(element){
+        if (element > 0){
+          return positives.push(element);
+        }
+      })
+    return positives.reduce((prev, curr) =>{
+      return prev + curr;},0)
+  }
+
+test1 = [1,2,3,4,5]
+test2 = [1,-2,3,4,5]
+test3 = [-1,-2,-3,-4,-5]
+test4 = [-1,2,3,4,-5]
+
+console.log(positiveSum(test1))
+console.log(positiveSum(test2))
+console.log(positiveSum(test3))
+console.log(positiveSum(test4))

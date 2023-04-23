0f10e38e94f31bc07e14de0c246342864ae1571f
Joel Diharce <joeldiharce@me.com>
2023-04-23 16:32:57 -0500

    uploading last react practice changes

--- react_js_practice/Basics/Practice/003_Creating_a_functional_Component/src/App.js
+++ react_js_practice/Basics/Practice/003_Creating_a_functional_Component/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import logo from './Assets/fi4oa3o2w7t51.png'
 import './App.css';
 
 // START notes file "001_Creating_a_functional_Component.md"
 function Header() {
-  return <h1>Hello world</h1>
+  return <h1 class="App-header">Hello world</h1>
+}
+
+function Artpic() {
+  return <img src={logo} alt="logo" class="App-logo"></img>
 }
 
 function App() {
-  return <Header />;
+  return <div class="App App-header"><Artpic /><Header /></div>
 }
 
 export default App;
